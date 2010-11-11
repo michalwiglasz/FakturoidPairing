7c057b4e0fdae5d35c01d18da93f536912c69bb0
Michal Wiglasz <michal.wiglasz@gmail.com>
2010-11-12 00:31:31 +0100

    Statement from email notifications support

--- app/Statement/EmailStatement.php
+++ app/Statement/EmailStatement.php
@@ -13,15 +13,74 @@
  */
 abstract class EmailStatement extends Statement {
 
+    protected $connection;
+
     public function __construct($configuration)
     {
         parent::construct($configuration);
-        //$serverReference, $mailbox, $username, $password
+        $this->serverReference = $configuration['mail_server'];
+
+        $this->connection = imap_open(
+                '{' . $configuration['mail_server'] . '}' . $configuration['mail_mailbox'],
+                $configuration['mail_username'],
+                $configuration['mail_password']
+        );
+
+        if(!$this->connection)
+                throw new Exception('Nepovedlo se připojit k poštovnímu serveru.');
     }
     
     public function getPayments() {
+        $payments = array();
 
+        $messageCount = imap_num_msg($this->connection);
+
+        for($i = 1; $i <= $messageCount; $i++)
+        {
+            $headers = imap_header($this->connection, $i);
+
+            if(($p = $this->processEmail($i, $headers)))
+            {
+                if(is_array($p))
+                    $payments = array_merge($payments, $p);
+                else
+                    $payments[] = $p;
+            }
+        }
+
+        return $payments;
     }
+
+    protected function fetchBody($msgno)
+    {
+        $body = imap_fetchbody($this->connection, $msgno, '1', FT_PEEK);
+
+        $structure = imap_fetchstructure($this->connection, $msgno);
+        $encoding = $structure->parts[0]->encoding;
+        $charset = null;
+        foreach($structure->parts[0]->parameters as $param)
+        {
+            if($param->attribute == 'CHARSET')
+            {
+                $charset = $param->value;
+                break;
+            }
+        }
+        
+        if($encoding == ENCBASE64)
+            $body = imap_base64($body);
+        elseif ($encoding == ENCQUOTEDPRINTABLE)
+            $body = imap_qprint($body);
+
+        if($charset)
+        {
+            $body = iconv($charset, "UTF-8", $body);
+        }
+
+        return $body;
+    }
+
+    abstract function processEmail($msgno, $headers);
 }
 
 
