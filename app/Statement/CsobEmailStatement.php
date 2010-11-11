f4271881043ce35d57ec4f84e5bf252a31577cb2
Michal Wiglasz <michal.wiglasz@gmail.com>
2010-11-12 00:31:50 +0100

    Statement from email notifications sent by CSOB

--- app/Statement/CsobEmailStatement.php
+++ app/Statement/CsobEmailStatement.php
@@ -12,7 +12,38 @@
  * @author Michal Wiglasz <michal.wiglasz@gmail.com>
  */
 class CsobEmailStatement extends EmailStatement {
-    
+
+    const REGEX = '#dne\s+([0-9]{1,2})\.([0-9]{1,2})\.(2[0-9]{3}).+(.|\n)+částka ([+-]?[0-9]+(,[0-9]+)?)(.|\n)+VS\s+([0-9]+)#ui';
+
+    function processEmail($msgno, $headers)
+    {
+        $from = $headers->from[0];
+
+        if($from->mailbox != 'administrator' || $from->host != 'tbs.csob.cz')
+                return NULL;
+
+        $body = $this->fetchBody($msgno);
+
+
+        $payments = array();
+        if(preg_match_all(self::REGEX, $body, $matches, PREG_SET_ORDER))
+        {
+            foreach($matches as $m)
+            {
+                $ammount = floatval(str_replace(',', '.', $m[5]));
+                if($ammount > 0)
+                {
+                    $payments[] = (object)array(
+                        'variable-symbol' => intval($m[8]),
+                        'date' => mktime(0,0,0, $m[2], $m[1], $m[3]),
+                        'ammount' => $ammount,
+                    );
+                }
+            }
+        }
+        
+        return count($payments) ? $payments : NULL;
+    }
 }
 
 
